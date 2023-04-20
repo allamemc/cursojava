f80f385c976e863da9a611d7cbc65776a92f3148
allamemc <allamemc@gmail.com>
2023-04-20 11:01:05 +0200

    curso

--- tema2/src/PrimitivosFloat.java
+++ tema2/src/PrimitivosFloat.java
@@ -1,5 +1,9 @@
 public class PrimitivosFloat {
     public static void main(String[] args) {
+        float realFloat = 1.5e4f; //15000f;
+        float realFloat2 = 1.5e-4f; //0.00015f;
 
+        System.out.println("realFloat = " + realFloat);
+        System.out.println("realFloat2 = " + realFloat2);
     }
 }
